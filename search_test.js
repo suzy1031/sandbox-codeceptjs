cca2e9f8b0201fc0276429447d681af0f0d2e70e
suzy1031 <kohei_sugawara@hotmail.co.jp>
2023-12-24 08:11:16 +0900

    Refactor search_test.js and add new scenario for button click

--- search_test.js
+++ search_test.js
@@ -1,10 +1,18 @@
 Feature("はじめてのCodeceptJS");
 
-Scenario("Googleで検索する", async ({ I }) => {
-  I.amOnPage("https://www.google.com");
-  I.fillField("#APjFqb", "cheese"); // #APjFqb -> textareaのid
-  I.pressKey("Enter");
+// 参考: https://miko.info/i_love_codeceptjs
+// Scenario("Googleで検索する", async ({ I }) => {
+//   I.amOnPage("https://www.google.com");
+//   I.fillField("#APjFqb", "cheese"); // #APjFqb -> textareaのid
+//   I.pressKey("Enter");
+//   I.wait(1);
+//   let firstResult = await I.grabTextFrom("h3");
+//   console.log(firstResult);
+// });
+
+Scenario("localhostでボタンをクリックする", async ({ I }) => {
+  I.amOnPage("http://localhost:5173/");
+  I.click("#count-up"); // #count-up -> buttonのid
   I.wait(1);
-  let firstResult = await I.grabTextFrom("h3");
-  console.log(firstResult);
+  I.see("count is 1", "#count-up"); // 第二引数に指定したid要素に第一引数の文字列が含まれているか
 });
