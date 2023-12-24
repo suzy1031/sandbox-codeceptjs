aff34a7b488c9b235939f5658e6ae03424b18c2e
suzy1031 <kohei_sugawara@hotmail.co.jp>
2023-12-24 09:04:04 +0900

    Refactor test scenarios and add comments

--- search_test.js
+++ search_test.js
@@ -1,6 +1,9 @@
 Feature("はじめてのCodeceptJS");
 
-// 参考: https://miko.info/i_love_codeceptjs
+/**
+ * 動作テスト シナリオ
+ * 参考: https://miko.info/i_love_codeceptjs
+ */
 // Scenario("Googleで検索する", async ({ I }) => {
 //   I.amOnPage("https://www.google.com");
 //   I.fillField("#APjFqb", "cheese"); // #APjFqb -> textareaのid
@@ -10,9 +13,30 @@ Feature("はじめてのCodeceptJS");
 //   console.log(firstResult);
 // });
 
-Scenario("localhostでボタンをクリックする", async ({ I }) => {
+/**
+ * local動作テスト シナリオ
+ * 参考: https://codecept.io/plugins/#customlocator
+ */
+// Scenario("localhostでカウントアップボタンをクリックする", async ({ I }) => {
+//   I.amOnPage("http://localhost:5173/");
+//   I.click("$count-up"); // #count-up -> buttonのcustom id
+//   I.wait(1);
+//   I.see("count is 1", "$count-up"); // 第二引数に指定したcustom id要素に第一引数の文字列が含まれているか
+// });
+
+Scenario("localhostでモーダルを開く", async ({ I }) => {
   I.amOnPage("http://localhost:5173/");
-  I.click("#count-up"); // #count-up -> buttonのid
+  I.click("$open-modal-button");
   I.wait(1);
-  I.see("count is 1", "#count-up"); // 第二引数に指定したid要素に第一引数の文字列が含まれているか
+  I.see("Use Google's location service?", "#alert-dialog-title"); // モーダルタイトルの表示を確認
+  I.see(
+    `Let Google help apps determine location. This means sending anonymous
+  location data to Google, even when no apps are running.`,
+    "#alert-dialog-description"
+  ); // モーダルメッセージの表示を確認
+  I.click("$right-button");
+  I.wait(1);
+  I.dontSee("Use Google's location service?"); // モーダルタイトルの非表示を確認
+  I.dontSee(`Let Google help apps determine location. This means sending anonymous
+  location data to Google, even when no apps are running.`); // モーダルメッセージの非表示を確認
 });
