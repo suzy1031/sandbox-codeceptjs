f146be7e35372cd189a05d2c02c1ba471035ccf2
suzy1031 <kohei_sugawara@hotmail.co.jp>
2023-12-29 07:42:18 +0900

    Update codecept.conf.js configuration

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,4 +1,4 @@
-const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+const { setHeadlessWhen, setCommonPlugins } = require("@codeceptjs/configure");
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
@@ -8,17 +8,32 @@ setCommonPlugins();
 
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: './*_test.js',
-  output: './output',
+  tests: "./*_test.js",
+  output: "./output",
   helpers: {
     Playwright: {
-      browser: 'chromium',
-      url: 'http://localhost',
-      show: true
-    }
+      browser: "chromium",
+      url: "http://localhost",
+      show: true,
+    },
   },
   include: {
-    I: './steps_file.js'
+    I: "./steps_file.js",
   },
-  name: 'sandbox-codeceptjs'
-}
+  plugins: {
+    stepByStepReport: {
+      enabled: true,
+      deleteSuccessful: false,
+      screenshotsForAllureReport: true,
+    },
+    customLocator: {
+      enabled: true,
+      attribute: "data-qa",
+    },
+    // 参考: https://codecept.io/plugins/#coverage
+    // coverage: {
+    //   enabled: true,
+    // },
+  },
+  name: "sandbox-codeceptjs",
+};
